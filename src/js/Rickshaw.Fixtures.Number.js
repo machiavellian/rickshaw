996e9d7a1155d7f5469a7e8ee988e3c4518c9928
Katie McLaughlin <katie@anchor.net.au>
2014-03-20 15:14:41 +1100

    be not silly

--- src/js/Rickshaw.Fixtures.Number.js
+++ src/js/Rickshaw.Fixtures.Number.js
@@ -11,15 +11,17 @@ Rickshaw.Fixtures.Number.formatKMBT = function(y) {
         else                      { return y }
 };
 
-
-Rickshaw.Fixtures.Number.formatKMBT_round = function(y) {
-        y_ = Rickshaw.Fixtures.Number.formatKMBT(y)
-        if (typeof y_ == "string") {
-                return y_
-        } else {
-                return y_.toFixed(2)
-        }
-};
+Rickshaw.Fixtures.Number.formatKMBT_round = function(y) { 
+        var abs_y = Math.abs(y);
+        var precision = 2;
+        if (abs_y >= 1000000000000)   { return (y / 1000000000000).toFixed(precision) + "T" }
+        else if (abs_y >= 1000000000) { return (y / 1000000000).toFixed(precision) + "B" }
+        else if (abs_y >= 1000000)    { return (y / 1000000).toFixed(precision) + "M" }
+        else if (abs_y >= 1000)       { return (y / 1000).toFixed(precision) + "K" }
+        else if (abs_y < 1 && y > 0)  { return y.toFixed(precision) }
+        else if (abs_y === 0)         { return 0 }
+        else                      { return y.toFixed(precision) }
+}; 
 
 Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
     var abs_y = Math.abs(y);
@@ -33,12 +35,15 @@ Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
     else                        { return y }
 };
 
-
 Rickshaw.Fixtures.Number.formatBase1024KMGTP_round = function(y) { 
-        y_ = Rickshaw.Fixtures.Number.formatBase1024KMGTP(y)
-        if (typeof y_ == "string") {
-                return y_
-        } else {
-                return y_.toFixed(2)
-        }
+    var abs_y = Math.abs(y);
+    var precision = 2;
+    if (abs_y >= 1125899906842624)  { return (y / 1125899906842624).toFixed(precision) + "P" }
+    else if (abs_y >= 1099511627776){ return (y / 1099511627776).toFixed(precision) + "T" }
+    else if (abs_y >= 1073741824)   { return (y / 1073741824).toFixed(precision) + "G" }
+    else if (abs_y >= 1048576)      { return (y / 1048576).toFixed(precision) + "M" }
+    else if (abs_y >= 1024)         { return (y / 1024).toFixed(precision) + "K" }
+    else if (abs_y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (abs_y === 0)           { return 0 }
+    else                      { return y.toFixed(precision) }
 };
